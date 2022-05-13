cfc328319c393b7a88d3ff8bd9e5657a3468b535
Mateo Florido <mateo.florido@canonical.com>
2022-05-13 15:07:35 -0500

    Created tests for test charm action

--- tests/integration/test_kubernetes_e2e.py
+++ tests/integration/test_kubernetes_e2e.py
@@ -11,11 +11,12 @@ import pytest
 import shlex
 from pytest_operator.plugin import OpsTest
 
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
 READY_MESSAGE = "Ready to test."
+TEST_ACTION_NAME = "test"
 
 
 @pytest.mark.abort_on_fail
@@ -24,29 +25,29 @@ async def test_build_and_deploy(ops_test: OpsTest):
     
     Assert on the unit status before any relations/configurations take place.
     """
-    logger.info("Build charm...")
+    log.info("Build charm...")
     charm = await ops_test.build_charm(".")
 
     overlays = [
-        ops_test.Bundle("kubernetes-core", channel="edge"),
+        ops_test.Bundle("charmed-kubernetes", channel="edge"),
         Path("tests/data/charm.yaml"),
     ]
 
-    logger.info("Rendering overlays...")
+    log.info("Rendering overlays...")
     bundle, *overlays = await ops_test.render_overlays(*overlays, charm=charm)
 
-    logger.info("Deploy charm...")
+    log.info("Deploy charm...")
     model = ops_test.model_full_name
 
     cmd = f"juju deploy -m {model} {bundle} " + " ".join(
         f"--overlay={f}" for f in overlays
     )
 
-    logger.info(cmd)
+    log.info(cmd)
     rc, stdout, stderr = await ops_test.run(*shlex.split(cmd))
     assert rc == 0, f"Bundle deploy failed: {(stderr or stdout).strip()}"
 
-    logger.info(stdout)
+    log.info(stdout)
     await ops_test.model.block_until(
         lambda: "kubernetes-e2e" in ops_test.model.applications, timeout=60
     )
@@ -60,5 +61,24 @@ async def test_build_and_deploy(ops_test: OpsTest):
         if not app.units:
             raise
 
-    # Check unit status
-    assert ops_test.model.applications[APP_NAME] == READY_MESSAGE
+    unit = ops_test.model.applications[APP_NAME].units[0]
+    # Check unit status message
+    assert unit.workload_status_message == READY_MESSAGE
+
+async def test_action_test(ops_test: OpsTest):
+    log.info("Queue action run...")
+    # Get application unit
+    unit = ops_test.model.applications[APP_NAME].units[0]
+    action = await unit.run_action(TEST_ACTION_NAME)
+
+    log.info("Wait for action...")
+    await action.wait()
+
+    log.info("Action finished...")
+    # Get action status from queued action id
+    result = await ops_test.model.get_action_status(uuid_or_prefix=action.entity_id)
+    assert result.get(action.entity_id, "") == "completed"
+
+
+
+
