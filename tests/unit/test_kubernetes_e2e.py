a85d78e4b190a924ae5eb33aced1d5c14171cafe
Etiene Audet-Cobello <etienne.audet-cobello@canonical.com>
2024-01-30 13:31:33 -0500

    Fix test failures caused by Conftest.py mocking charms

--- tests/unit/test_kubernetes_e2e.py
+++ tests/unit/test_kubernetes_e2e.py
@@ -1,7 +1,8 @@
-import charm
+from charm import OpsCharmKubernetesE2ECharm
+from charm import determine_arch
 import unittest.mock as mock
 
 def test_determine_arch():
-    with mock.patch('kubernetes_e2e.check_output') as mock_check_output:
+    with mock.patch('subprocess.check_output') as mock_check_output:
         mock_check_output.return_value.decode.return_value = "amd64\n"
-        assert charm.determine_arch() == "amd64"
+        assert determine_arch() == "amd64"
