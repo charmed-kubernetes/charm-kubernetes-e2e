68a19685b2b092fc18decfe2deaaecdc9e4014cf
eaudetcobello <etienne.audet-cobello@canonical.com>
2024-02-06 22:09:08 +0000

    Review changes

--- src/charm.py
+++ src/charm.py
@@ -12,7 +12,7 @@ from pathlib import Path
 from typing import Optional
 
 import ops
-from charms.operator_libs_linux.v2.snap import SnapCache, SnapState
+from charms.operator_libs_linux.v2 import snap
 from ops import (
     ActionEvent,
     BlockedStatus,
@@ -49,8 +49,6 @@ class KubernetesE2ECharm(ops.CharmBase):
 
         self.CA_CERT_PATH.parent.mkdir(exist_ok=True)
 
-        self.snap_cache = SnapCache()
-
         kubecontrol = self.on["kube-control"]
         self.framework.observe(kubecontrol.relation_broken, self._setup_environment)
         self.framework.observe(kubecontrol.relation_joined, self._kube_control_relation_joined)
@@ -61,10 +59,8 @@ class KubernetesE2ECharm(ops.CharmBase):
         self.framework.observe(certificates.relation_created, self._setup_environment)
         self.framework.observe(certificates.relation_changed, self._setup_environment)
 
-        self.framework.observe(self.on.start, self._on_start)
         self.framework.observe(self.on.test_action, self._on_test_action)
         self.framework.observe(self.on.config_changed, self._setup_environment)
-        self.framework.observe(self.on.upgrade_charm, self._upgrade_charm)
 
     def _kube_control_relation_joined(self, event: EventBase):
         self.kube_control.set_auth_request(self.unit.name)
@@ -115,33 +111,11 @@ class KubernetesE2ECharm(ops.CharmBase):
 
         self.unit.status = ops.ActiveStatus("Ready to test.")
 
-    def _upgrade_charm(self, event: EventBase) -> None:
-        channel = self.config.get("channel")
-        self._install_snaps(channel)
-
     def _install_snaps(self, channel: Optional[str]) -> None:
-        self.unit.status = ops.MaintenanceStatus("Installing kubectl snap.")
-        self._ensure_snap("kubectl", channel=channel)
-
-        self.unit.status = ops.MaintenanceStatus("Installing kubernetes-test snap.")
-        self._ensure_snap("kubernetes-test", channel=channel, classic=True)
-
-    def _ensure_snap(
-        self,
-        name: str,
-        state: SnapState = SnapState.Latest,
-        channel: Optional[str] = "",
-        classic: Optional[bool] = False,
-    ) -> None:
-        if not isinstance(name, str) or name == "":
-            raise ValueError("A name is required to ensure a snap.")
-
-        snap = self.snap_cache[name]
-        if not snap.present:
-            snap.ensure(state=state, classic=classic, channel=channel)
-
-    def _on_start(self, event: EventBase) -> None:
-        self.unit.status = ops.ActiveStatus()
+        self.unit.status = ops.MaintenanceStatus("Installing kubectl and kubernetes-test snaps.")
+        snap.ensure("kubectl", snap.SnapState.Latest.value, channel=channel)
+        snap.ensure("kubernetes-test", snap.SnapState.Latest.value, channel=channel, classic=True)
+        self.unit.status = ops.MaintenanceStatus("Snaps installed successfully.")
 
     def _check_kube_config_exists(self, event: ActionEvent) -> bool:
         if not Path(KUBE_CONFIG_PATH).exists():
